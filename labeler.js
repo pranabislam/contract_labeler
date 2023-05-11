c46294bd7be1eb83437abe812e3867037c5575c1
Pranab Islam <islampranab@gmail.com>
2023-05-10 23:36:03 -0400

    Update labeler.js
    working dialog box and file saver

--- labeler.js
+++ labeler.js
@@ -75,83 +75,54 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
-// Define the options for the popup menu
-const options = ["Option 1", "Option 2", "Option 3"];
 
-// Create the popup menu element
-const menu = document.createElement("div");
-menu.classList.add("popup-menu");
 
-// Create an option element for each option
-options.forEach((option, index) => {
-  const optionElement = document.createElement("div");
-  optionElement.classList.add("popup-menu-option");
-  optionElement.textContent = option;
-  optionElement.dataset.index = index;
-  menu.appendChild(optionElement);
-});
-
-// Add the menu to the document
-document.body.appendChild(menu);
-
-// Hide the menu initially
-menu.style.display = "none";
-
-// Track the currently selected option
+let isMenuOpen = false;
+let mouseX;
+let mouseY;
 let selectedOption = null;
 
-// Handle the 'u' key press
 document.addEventListener("keydown", (event) => {
-  if (event.key === "u") {
-    // Show the menu
-    menu.style.display = "block";
-    console.log('In pop up menu!')
+  if (event.key === "u" && !isMenuOpen) {
+    isMenuOpen = true;
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+    console.log('in event u!')
+    const menuWindow = window.open("", "Dialog Box", `width=400,height=200,top=${mouseY},left=${mouseX}`);
+    const dialog = menuWindow.document.createElement("div");
+    dialog.style.display = "flex";
+    dialog.style.flexDirection = "column";
+    dialog.style.justifyContent = "center";
+    dialog.style.alignItems = "center";
+    menuWindow.document.body.appendChild(dialog);
 
-    // Position the menu at the current mouse cursor position
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    const menuWidth = menu.offsetWidth;
-    const menuHeight = menu.offsetHeight;
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    const message = menuWindow.document.createElement("p");
+    message.textContent = "Please select an option:";
+    message.style.fontSize = "20px";
+    dialog.appendChild(message);
 
-    let menuX = mouseX;
-    let menuY = mouseY;
-
-    if (menuX + menuWidth > windowWidth) {
-      menuX = windowWidth - menuWidth;
-    }
-
-    if (menuY + menuHeight > windowHeight) {
-      menuY = windowHeight - menuHeight;
-    }
-
-    menu.style.left = menuX + "px";
-    menu.style.top = menuY + "px";
-
-    // Add a click event listener to the menu options
-    menu.querySelectorAll(".popup-menu-option").forEach((option) => {
-      option.addEventListener("click", () => {
-        // Store the selected option and hide the menu
-        selectedOption = options[option.dataset.index];
-        menu.style.display = "none";
-      });
+    const option1 = menuWindow.document.createElement("button");
+    option1.textContent = "Option 1";
+    option1.style.margin = "10px";
+    option1.addEventListener("click", () => {
+      selectedOption = "Option 1";
+      isMenuOpen = false;
+      menuWindow.close();
     });
+    dialog.appendChild(option1);
+
+    const option2 = menuWindow.document.createElement("button");
+    option2.textContent = "Option 2";
+    option2.style.margin = "10px";
+    option2.addEventListener("click", () => {
+      selectedOption = "Option 2";
+      isMenuOpen = false;
+      menuWindow.close();
+    });
+    dialog.appendChild(option2);
   }
 });
 
-// Add some basic CSS styles for the menu and options
-menu.style.position = "absolute";
-menu.style.padding = "10px";
-menu.style.backgroundColor = "#fff";
-menu.style.border = "1px solid #000";
-menu.style.width = "200px";
-menu.style.zIndex = 100;
-
-menu.querySelectorAll(".popup-menu-option").forEach((option) => {
-  option.style.padding = "5px";
-  option.style.cursor = "pointer";
-});
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'b') {
